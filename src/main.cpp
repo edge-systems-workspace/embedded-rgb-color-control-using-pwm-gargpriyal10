e1acbb9fb8755392abee9dc03a773663bc7a4e68
gargpriyal10 <priyal.garg_cs.aiml24@gla.ac.in>
2026-02-19 15:45:56 +0530

    add XML configuration files and update color pin definitions in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,9 @@
  * and analog PWM brightness control.
  */
 
- // TODO 1:
- // Define RED pin (Use 9)
-
- // TODO 2:
- // Define GREEN pin (Use 10)
-
- // TODO 3:
- // Define BLUE pin (Use 11)
+int red = 1;
+int blue= 3;
+int green =2;
 
 void setup() {
 
